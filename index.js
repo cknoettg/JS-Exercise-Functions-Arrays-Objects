b763fe00334e162ac85b36b28da2513e85f92a85
Corey Knoettgen <cknoettg@yahoo.com>
2020-02-09 19:32:50 -0500

    Add getOlderCars

--- index.js
+++ index.js
@@ -222,8 +222,13 @@ function sortCarInventory(inventory) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * getModelYears returns an array containing all the 'car_year's in the inventory.
 */
-function getModelYears(/* code here */) {
+function getModelYears(inventory) {
   /* code here */
+  let years =[];
+  for (let i = 0; i < inventory.length; i++) {
+    years[i]=inventory[i].car_year;
+  }
+  return years;
 }
 
 /**
@@ -238,8 +243,17 @@ function getModelYears(/* code here */) {
  * with a `car_year` which is at most the given desired max year,
  * in the same order as they appear in the original inventory.
 */
-function getOlderCars(/* code here */) {
+function getOlderCars(inventory,maxYear) {
   /* code here */
+  let oldest=[];
+  let next=0;
+  for (let i = 0; i < inventory.length; i++) {
+    if (maxYear >= inventory[i].car_year){
+      oldest[next]=inventory[i];
+      next++;
+    }
+  }
+  return oldest;
 }
 
 /**
